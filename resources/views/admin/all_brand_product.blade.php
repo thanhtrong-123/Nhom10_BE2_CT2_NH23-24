b62c46320dd3192a869b3efbcb59b06bebd6a1dc
thanhtrong-123 <91948049+thanhtrong-123@users.noreply.github.com>
2024-03-28 15:59:13 +0700

    Update all_brand_product.blade.php

--- resources/views/admin/all_brand_product.blade.php
+++ resources/views/admin/all_brand_product.blade.php
@@ -26,14 +26,7 @@
         </div>
       </div>
     </div>
-    <div class="table-responsive">
-                      <?php
-                            $message = Session::get('message');
-                            if($message){
-                                echo '<span class="text-alert">'.$message.'</span>';
-                                Session::put('message',null);
-                            }
-                            ?>
+    <div class="table-responsive"> 
       <table class="table table-striped b-t b-light">
         <thead>
           <tr>
@@ -50,34 +43,24 @@
           </tr>
         </thead>
         <tbody>
-          @foreach($all_brand_product as $key => $brand_pro)
+          
           <tr>
             <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
-            <td>{{ $brand_pro->brand_name }}</td>
-            <td>{{ $brand_pro->brand_slug }}</td>
+            <td></td>
+            <td></td>
             <td><span class="text-ellipsis">
-              <?php
-               if($brand_pro->brand_status==0){
-                ?>
-                <a href="{{URL::to('/unactive-brand-product/'.$brand_pro->brand_id)}}"><span class="fa-thumb-styling fa fa-thumbs-up"></span></a>
-                <?php
-                 }else{
-                ?>  
-                 <a href="{{URL::to('/active-brand-product/'.$brand_pro->brand_id)}}"><span class="fa-thumb-styling fa fa-thumbs-down"></span></a>
-                <?php
-               }
-              ?>
+              
             </span></td>
            
             <td>
-              <a href="{{URL::to('/edit-brand-product/'.$brand_pro->brand_id)}}" class="active styling-edit" ui-toggle-class="">
+              <a href="" class="active styling-edit" ui-toggle-class="">
                 <i class="fa fa-pencil-square-o text-success text-active"></i></a>
-              <a onclick="return confirm('Bạn có chắc là muốn xóa thương hiệu này ko?')" href="{{URL::to('/delete-brand-product/'.$brand_pro->brand_id)}}" class="active styling-edit" ui-toggle-class="">
+              <a onclick="return confirm('Bạn có chắc là muốn xóa thương hiệu này ko?')" href="" class="active styling-edit" ui-toggle-class="">
                 <i class="fa fa-times text-danger text"></i>
               </a>
             </td>
           </tr>
-          @endforeach
+          
         </tbody>
       </table>
     </div>
@@ -89,7 +72,7 @@
         </div>
         <div class="col-sm-7 text-right text-center-xs">                
           <ul class="pagination pagination-sm m-t-none m-b-none">
-             {!!$all_brand_product->links()!!}
+             
           </ul>
         </div>
       </div>
